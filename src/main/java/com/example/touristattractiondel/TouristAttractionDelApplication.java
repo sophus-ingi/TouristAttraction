df34e3e966a800645e02e0366e507d416c499f31
Sophus Ingi <Sophussophusson@gmail.com>
2024-02-13 09:18:33 +0100

    Initial commit

---
+++ src/main/java/com/example/touristattractiondel/TouristAttractionDelApplication.java
@@ -0,0 +1,13 @@
+package com.example.touristattractiondel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristAttractionDelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TouristAttractionDelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/touristattractiondel/TouristAttractionDelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.touristattractiondel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristAttractionDelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
